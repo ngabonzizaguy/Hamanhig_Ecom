52e49204b0f84af591fa2c8129a015e3b55d1ec1
ngabonzizaguy <ngabonzizaguy1996@gmail.com>
2024-08-24 20:50:03 +0000

    Initial commit

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,15 +1,34 @@
 "use client"
 
-import Image from "next/image"
+import Image from "next/image";
+import { useRouter } from "next/navigation"
 
 // import React from 'react'
 
 const SearchBar = () => {
+
+  const router = useRouter();
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    
+    const formData = new FormData(e.currentTarget);
+    const name = formData.get("name") as String;
+
+    if(name) {
+      router.push(`/list?name=${name}`);
+    }
+  }
+
   return (
-    <form className="flex ic justify-between gap-4 bg-gray-100 p-2 rounded-md flex-1" onSubmit={}>
-      <input type="text" placeholder="Search" className="flex-1 bg-transparent outline-none" />
+    <form className="flex ic justify-between gap-4 bg-gray-100 p-2 rounded-md flex-1" onSubmit={handleSearch}>
+      <input 
+      type="text" 
+      name="name" 
+      placeholder="Search" 
+      className="flex-1 bg-transparent outline-none" />
+
       <button className="cursor-pointer">
-        <Image src="/search.png" alt={"search"} width={16} height={16} />
+        <Image src="/search.png" alt={"search"} width={16} height={16}></Image>
       </button>
     </form>  
 
