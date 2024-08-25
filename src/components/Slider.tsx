fd301d41009639935e1e7a7b2c7f31729af86a69
ngabonzizaguy <ngabonzizaguy1996@gmail.com>
2024-08-25 10:24:17 +0000

    hero bullet slides

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -37,17 +37,25 @@ const Slider = () => {
     <div className="h-[calc(100vh-80px)] overflow-hidden">
       <div className="w-max h-full flex transition-all ease-in-out duration-1000">
         {slides.map((slide) => (
-          <div className={`$(slide.bg) w-screen h-full flex flex-col gap-16 xl:flex-row`} key={slide.id}>
+          <div
+            className={`$(slide.bg) w-screen h-full flex flex-col gap-16 xl:flex-row`}
+            key={slide.id}
+          >
             {/* TEXT CONTAINER */}
-            <div className="h-1/2 xl:w-1/2 xl:h-full flex flex-col items-center justify-between gap-8 2xl:gap-12 text-center">
-              <h2 className="text-xl lg:text-3xl 2xl:text-5xl">{slide.description}</h2>
-              <h1 className="text-5xl lg:text-6xl 2xl:text-8xl font-semibold">{slide.title}</h1>
+            <div className="h-1/2 xl:w-1/2 xl:h-full flex flex-col items-center justify-center gap-8 2xl:gap-12 text-center">
+              <h2 className="text-xl lg:text-3xl 2xl:text-5xl">
+                {slide.description}
+              </h2>
+              <h1 className="text-5xl lg:text-6xl 2xl:text-8xl font-semibold">
+                {slide.title}
+              </h1>
               <Link href={slide.url}>
                 <button className="rounded-md bg-black text-white py-3 px-4">
-                    SHOP NOW
+                  SHOP NOW
                 </button>
               </Link>
             </div>
+
             {/* IMAGE CONTAINER */}
             <div className="h-1/2 xl:w-1/2 xl:h-full relative">
               <Image
@@ -57,6 +65,21 @@ const Slider = () => {
                 sizes="100%"
                 className="object-cover"
               ></Image>
+            </div>
+
+            <div className="">
+              {slides.map((slide, index) => (
+                <div
+                  className={`w-3 h-3 rounded-full ring-1 ring-gray-600 cursor-pointer flex items-center justify-center ${
+                    current === index ? "scale-150" : ""
+                  }`}
+                  key={slide.id}
+                >
+                  {current === index && (
+                    <div className="w-[6px] h-[6px] bg-gray-600 rounded-full"></div>
+                  )}
+                </div>
+              ))}
             </div>
           </div>
         ))}
