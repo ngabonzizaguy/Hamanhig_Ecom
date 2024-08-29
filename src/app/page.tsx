541e96d6a2b6fe1da2ce110e2e5de4f09e62f88e
N.Guy <ngabonzizaguy1996@gmail.com>
2024-08-29 11:37:15 +0000

    initial commit - slider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import ProductList from "@/components/ProductList"
 import Slider from "@/components/Slider"
 
 const HomePage = () => {
   return (
     <div className=''>
       <Slider/>
+      <ProductList/>
     </div>
   )
 }
--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import React, { useState } from "react";
+import { useEffect, useState } from "react";
 
 const slides = [
   {
@@ -33,12 +33,24 @@ const slides = [
 
 const Slider = () => {
   const [current, setCurrent] = useState(0);
+
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     setCurrent((prev) => (prev === slides.length - 1 ? 0 : prev + 1));
+  //   }, 3000);
+
+  //   return () => clearInterval(interval);
+  // }, []);
+
   return (
     <div className="h-[calc(100vh-80px)] overflow-hidden">
-      <div className="w-max h-full flex transition-all ease-in-out duration-1000">
+      <div
+        className="w-max h-full flex transition-all ease-in-out duration-1000"
+        style={{ transform: `translateX(-${current * 100}vw)` }}
+      >
         {slides.map((slide) => (
           <div
-            className={`$(slide.bg) w-screen h-full flex flex-col gap-16 xl:flex-row`}
+            className={`${slide.bg} w-screen h-full flex flex-col gap-16 xl:flex-row`}
             key={slide.id}
           >
             {/* TEXT CONTAINER */}
@@ -50,12 +62,11 @@ const Slider = () => {
                 {slide.title}
               </h1>
               <Link href={slide.url}>
-                <button className="rounded-md bg-black text-white py-3 px-4">
+                <button className="rounded-md bg-black text-white py-3 px-4 ">
                   SHOP NOW
                 </button>
               </Link>
             </div>
-
             {/* IMAGE CONTAINER */}
             <div className="h-1/2 xl:w-1/2 xl:h-full relative">
               <Image
@@ -64,23 +75,23 @@ const Slider = () => {
                 fill
                 sizes="100%"
                 className="object-cover"
-              ></Image>
+              />
             </div>
-
-            <div className="">
-              {slides.map((slide, index) => (
-                <div
-                  className={`w-3 h-3 rounded-full ring-1 ring-gray-600 cursor-pointer flex items-center justify-center ${
-                    current === index ? "scale-150" : ""
-                  }`}
-                  key={slide.id}
-                >
-                  {current === index && (
-                    <div className="w-[6px] h-[6px] bg-gray-600 rounded-full"></div>
-                  )}
-                </div>
-              ))}
-            </div>
+          </div>
+        ))}
+      </div>
+      <div className="absolute m-auto left-1/2 bottom-8 flex gap-4">
+        {slides.map((slide, index) => (
+          <div
+            className={`w-3 h-3  rounded-full ring-1 ring-gray-600 cursor-pointer flex items-center justify-center ${
+              current === index ? "scale-150" : ""
+            }`}
+            key={slide.id}
+            onClick={() => setCurrent(index)}
+          >
+            {current === index && (
+              <div className="w-[6px] h-[6px] bg-gray-600 rounded-full"></div>
+            )}
           </div>
         ))}
       </div>
