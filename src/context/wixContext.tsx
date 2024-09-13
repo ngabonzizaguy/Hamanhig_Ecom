3e9b9594c2f427ce23216571e2109255723de6e7
N.Guy <32185091+ngabonzizaguy@users.noreply.github.com>
2024-09-13 14:36:26 +0000

    wixcontext - added shToken token

--- src/context/wixContext.tsx
+++ src/context/wixContext.tsx
@@ -2,6 +2,9 @@
 
 import { createClient, OAuthStrategy } from "@wix/sdk";
 import {products, categories} from "@wix/stores"
+import Cookies from "js-cookie";
+
+const refreshToken = JSON.parse(Cookies.get("refreshToken") || "{}")
 
 const myWixClient = createClient({
     modules: {
@@ -11,13 +14,7 @@ const myWixClient = createClient({
     auth: OAuthStrategy({
       clientId: process.env.NEXT_PUBLIC_WIX_CLIENT_ID!,
       tokens: {
-        accessToken: {
-          value: "<ACCESS_TOKEN_VALUE>",
-          expiresAt: "<ACCESS_TOKEN_EXPIRY_DATE>",
-        },
-        refreshToken: {
-          value: "<REFRESH_TOKEN_VALUE>",
-        },
+        refreshToken, accessToken: {value: "", expiresAt: 0},
       },
     }),
   });
