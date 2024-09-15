8665c81ed381878c9378ef3ea5a4a5ce4e0a8ff3
N.Guy <32185091+ngabonzizaguy@users.noreply.github.com>
2024-09-15 15:24:14 +0000

    fetching wix server data(products))

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,128 +1,51 @@
+import { wixClientServer } from '@/lib/wixClientServer';
 import Image from 'next/image';
+import { products } from "@wix/stores";
 import Link from 'next/link';
 
-const ProductList = () => {
+
+const PRODUCT_PER_PAGE = 20
+
+const ProductList = async ({categoryId, limit}:{categoryId:string; limit?:number;}) => {
+
+  const wixClient = await wixClientServer();
+
+  const res = await wixClient.products.queryProducts().eq("collectionIds",categoryId).limit(limit || PRODUCT_PER_PAGE).find();
+
+  console.log(res.items[0].priceData)
   return (
     <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap">
-      <Link
-        href="/test"
-        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
-      >
+      {res.items.map((product: products.Product) => (
+        <Link
+        href={"/"+product.slug}
+        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]" key={product._id}
+        >
         <div className="relative w-full h-80">
           <Image
-            src="https://images.pexels.com/photos/1183266/pexels-photo-1183266.jpeg?auto=compress&cs=tinysrgb&w=800"
+            src={product.media?.mainMedia?.image?.url || "/product.png"}
             alt=""
             fill
             sizes="25vw"
             className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
-          />
-          <Image
-            src="https://images.pexels.com/photos/1021693/pexels-photo-1021693.jpeg?auto=compress&cs=tinysrgb&w=800"
+            />
+          { product.media?.items && <Image
+            src={product.media?.items[1]?.image?.url || "/product.png"}
             alt=""
             fill
             sizes="25vw"
             className="absolute object-cover rounded-md"
-          />
+          />}
         </div>
         <div className="flex justify-between">
-          <span className="font-medium">Product Name</span>
-          <span className="font-semibold">$49</span>
+          <span className="font-medium">{product.name}</span>
+          <span className="font-semibold">{product.priceData?.price}</span>
         </div>
         <div className="text-sm text-gray-500">Description</div>
         <button className="rounded-2xl ring-1 ring-theme1 w-max text-theme1 py-2 px-4 text-xs hover:bg-theme1 hover:text-white">
           Add to Cart
         </button>
       </Link>
-
-      <Link
-        href="/test"
-        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
-      >
-        <div className="relative w-full h-80">
-          <Image
-            src="https://images.pexels.com/photos/1183266/pexels-photo-1183266.jpeg?auto=compress&cs=tinysrgb&w=800"
-            alt=""
-            fill
-            sizes="25vw"
-            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
-          />
-          <Image
-            src="https://images.pexels.com/photos/1021693/pexels-photo-1021693.jpeg?auto=compress&cs=tinysrgb&w=800"
-            alt=""
-            fill
-            sizes="25vw"
-            className="absolute object-cover rounded-md"
-          />
-        </div>
-        <div className="flex justify-between">
-          <span className="font-medium">Product Name</span>
-          <span className="font-semibold">$49</span>
-        </div>
-        <div className="text-sm text-gray-500">Description</div>
-        <button className="rounded-2xl ring-1 ring-theme1 w-max text-theme1 py-2 px-4 text-xs hover:bg-theme1 hover:text-white">
-          Add to Cart
-        </button>
-      </Link>
-
-      <Link
-        href="/test"
-        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
-      >
-        <div className="relative w-full h-80">
-          <Image
-            src="https://images.pexels.com/photos/1183266/pexels-photo-1183266.jpeg?auto=compress&cs=tinysrgb&w=800"
-            alt=""
-            fill
-            sizes="25vw"
-            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
-          />
-          <Image
-            src="https://images.pexels.com/photos/1021693/pexels-photo-1021693.jpeg?auto=compress&cs=tinysrgb&w=800"
-            alt=""
-            fill
-            sizes="25vw"
-            className="absolute object-cover rounded-md"
-          />
-        </div>
-        <div className="flex justify-between">
-          <span className="font-medium">Product Name</span>
-          <span className="font-semibold">$49</span>
-        </div>
-        <div className="text-sm text-gray-500">Description</div>
-        <button className="rounded-2xl ring-1 ring-theme1 w-max text-theme1 py-2 px-4 text-xs hover:bg-theme1 hover:text-white">
-          Add to Cart
-        </button>
-      </Link>
-
-      <Link
-        href="/test"
-        className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]"
-      >
-        <div className="relative w-full h-80">
-          <Image
-            src="https://images.pexels.com/photos/1183266/pexels-photo-1183266.jpeg?auto=compress&cs=tinysrgb&w=800"
-            alt=""
-            fill
-            sizes="25vw"
-            className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500"
-          />
-          <Image
-            src="https://images.pexels.com/photos/1021693/pexels-photo-1021693.jpeg?auto=compress&cs=tinysrgb&w=800"
-            alt=""
-            fill
-            sizes="25vw"
-            className="absolute object-cover rounded-md"
-          />
-        </div>
-        <div className="flex justify-between">
-          <span className="font-medium">Product Name</span>
-          <span className="font-semibold">$49</span>
-        </div>
-        <div className="text-sm text-gray-500">Description</div>
-        <button className="rounded-2xl ring-1 ring-theme1 w-max text-theme1 py-2 px-4 text-xs hover:bg-theme1 hover:text-white">
-          Add to Cart
-        </button>
-      </Link>
+    ))}
     </div>
   );
 };
