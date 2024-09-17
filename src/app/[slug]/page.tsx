ee89f55586014f979c6a27dd04ef647023588f62
N.Guy <32185091+ngabonzizaguy@users.noreply.github.com>
2024-09-17 11:10:54 +0000

    product slug

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,21 +1,36 @@
 import ProductImages from '@/components/ProductImages';
 import CustomizeProducts from '@/components/CustomizeProducts';
 import Add from '@/components/Add';
+import { wixClientServer } from '@/lib/wixClientServer';
 
-const SinglePage = () => {
+const SinglePage = async ({params}:{params:{slug:string}}) => {
+
+  console.log(params.slug)
+
+  const wixClient = await wixClientServer();
+
+  const products = await wixClient.products
+  .queryProducts()
+  .eq("slug", params.slug)
+  .find();
+
+  if(!products.items[0]) {
+    return notFound()
+  }
+
+  const product = products.items[0]
+
   return (
     <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative flex flex-col lg:flex-row gap-16">
       {/* IMGS */}
       <div className="w-full lg:w-1/2 lg:sticky top-20 h-max">
-        <ProductImages />
+        <ProductImages items={product.media?.items} />
       </div>
       {/* TEXTS */}
       <div className="w-full lg:w-1/2 flex flex-col gap-6">
-        <h1 className="text-4xl font-medium">Product Name</h1>
+        <h1 className="text-4xl font-medium">{product.name}</h1>
         <p className="text-gray-500">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-          Perspiciatis, natus!amet consectetur adipisicing elit. Perspiciatis,
-          natus!
+          {product.description}
         </p>
         <div className="h-[2px] bg-gray-100" />
         <div className="flex items-center gap-4">
