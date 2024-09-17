7a2675820e76789ed86286bca6ec52c6acaf95f4
N.Guy <32185091+ngabonzizaguy@users.noreply.github.com>
2024-09-17 16:05:54 +0000

    Added products listings

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -2,24 +2,26 @@ import ProductImages from '@/components/ProductImages';
 import CustomizeProducts from '@/components/CustomizeProducts';
 import Add from '@/components/Add';
 import { wixClientServer } from '@/lib/wixClientServer';
+import { notFound } from 'next/navigation';
 
-const SinglePage = async ({params}:{params:{slug:string}}) => {
+const SinglePage = async ({ params }: { params: { slug: string } }) => {
 
   console.log(params.slug)
 
   const wixClient = await wixClientServer();
 
   const products = await wixClient.products
-  .queryProducts()
-  .eq("slug", params.slug)
-  .find();
+    .queryProducts()
+    .eq("slug", params.slug)
+    .find();
 
-  if(!products.items[0]) {
+  if (!products.items[0]) {
     return notFound()
   }
 
   const product = products.items[0]
 
+  console.log(product.variants)
   return (
     <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative flex flex-col lg:flex-row gap-16">
       {/* IMGS */}
@@ -33,38 +35,27 @@ const SinglePage = async ({params}:{params:{slug:string}}) => {
           {product.description}
         </p>
         <div className="h-[2px] bg-gray-100" />
-        <div className="flex items-center gap-4">
-          <h3 className="text-xl text-gray-500 line-through">$59</h3>
-          <h2 className="font-medium text-2xl">$49</h2>
-        </div>
+        {product.priceData?.price === product.priceData?.discountedPrice ? (<h2 className="font-medium text-2xl">${product.priceData?.price}</h2>) : (
+          <div className="flex items-center gap-4">
+            <h3 className="text-xl text-gray-500 line-through">${product.priceData?.price}</h3>
+            <h2 className="font-medium text-2xl">${product.priceData?.discountedPrice}</h2>
+          </div>
+        )}
         <div className="h-[2px] bg-gray-100" />
         <CustomizeProducts />
         <Add />
         <div className="h-[2px] bg-gray-100" />
-        <div className="text-sm">
-          <h4 className="font-medium mb-4">Title</h4>
+        {product.additionalInfoSections?.map((section:any) => (
+
+          <div className="text-sm" key={section.title}>
+          <h4 className="font-medium mb-4">{section.title}</h4>
           <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt, nam
-            nulla dolore in mollitia culpa a voluptatem commodi suscipit
-            obcaecati. mollitia culpa a voluptatem commodi suscipit obcaecati.
+            {section.description}
           </p>
-        </div>
-        <div className="text-sm">
-          <h4 className="font-medium mb-4">Title</h4>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt, nam
-            nulla dolore in mollitia culpa a voluptatem commodi suscipit
-            obcaecati. mollitia culpa a voluptatem commodi suscipit obcaecati.
-          </p>
-        </div>
-        <div className="text-sm">
-          <h4 className="font-medium mb-4">Title</h4>
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt, nam
-            nulla dolore in mollitia culpa a voluptatem commodi suscipit
-            obcaecati. mollitia culpa a voluptatem commodi suscipit obcaecati.
-          </p>
-        </div>
+          </div>
+        ))
+      }
+          
       </div>
     </div>
   );
