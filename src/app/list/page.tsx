d082d618ca86d588255d7be5374564a35d36bca5
N.Guy <32185091+ngabonzizaguy@users.noreply.github.com>
2024-09-20 14:23:15 +0000

    Added product filtering feature

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,16 +1,19 @@
-import Filter from '@/components/Filter';
-import ProductList from '@/components/ProductList';
-import { wixClientServer } from '@/lib/wixClientServer';
-import Image from 'next/image';
-import { Suspense } from 'react';
+import Filter from "@/components/Filter";
+import ProductList from "@/components/ProductList";
+import Skeleton from "@/components/Skeleton";
+import { wixClientServer } from "@/lib/wixClientServer";
+import Image from "next/image";
+import { Suspense } from "react";
 
 const ListPage = async ({ searchParams }: { searchParams: any }) => {
+  const wixClient = await wixClientServer();
 
-  const wixClient = await wixClientServer()
-  const cat = await wixClient.collections.getCollectionBySlug(searchParams.cat || "all-products");
+  const cat = await wixClient.collections.getCollectionBySlug(
+    searchParams.cat || "all-products"
+  );
 
   return (
-    <div className="px-4 md:px-8 lg:px-16 xl:32 2xl:px-64 relative">
+    <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 relative">
       {/* CAMPAIGN */}
       <div className="hidden bg-pink-50 px-4 sm:flex justify-between h-64">
         <div className="w-2/3 flex flex-col items-center justify-center gap-8">
@@ -18,25 +21,25 @@ const ListPage = async ({ searchParams }: { searchParams: any }) => {
             Grab up to 50% off on
             <br /> Selected Products
           </h1>
-          <button className="rounded-3xl bg-theme1 text-white w-max py-3 px-5 text-sm">
+          <button className="rounded-3xl bg-lama text-white w-max py-3 px-5 text-sm">
             Buy Now
           </button>
         </div>
-        <div className="relative w-2/3">
-          <Image
-            src="/woman.png"
-            alt="woman image"
-            fill={true}
-            className="object-contain"
-          ></Image>
+        <div className="relative w-1/3">
+          <Image src="/woman.png" alt="" fill className="object-contain" />
         </div>
       </div>
       {/* FILTER */}
       <Filter />
       {/* PRODUCTS */}
-      <h1 className="mt-12 text-xl font-semibold">Shoes For you</h1>
-      <Suspense fallback={"loading..."}>
-        <ProductList categoryId={cat.collection?._id || "00000000-000000-000000-000000000001"} searchParams={searchParams}/>
+      <h1 className="mt-12 text-xl font-semibold">{cat?.collection?.name} For You!</h1>
+      <Suspense fallback={<Skeleton/>}>
+        <ProductList
+          categoryId={
+            cat.collection?._id || "00000000-000000-000000-000000000001"
+          }
+          searchParams={searchParams}
+        />
       </Suspense>
     </div>
   );
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,21 +1,56 @@
-import { wixClientServer } from '@/lib/wixClientServer';
-import Image from 'next/image';
+import { wixClientServer } from "@/lib/wixClientServer";
 import { products } from "@wix/stores";
+import Image from "next/image";
+import Link from "next/link";
 import DOMPurify from "isomorphic-dompurify";
-import Link from 'next/link';
+// import Pagination from "./Pagination";
 
+const PRODUCT_PER_PAGE = 8;
 
-const PRODUCT_PER_PAGE = 20
-
-const ProductList = async ({categoryId, limit, searchParams}:{categoryId:string; limit?:number; searchParams?:any}) => {
-
+const ProductList = async ({
+  categoryId,
+  limit,
+  searchParams,
+}: {
+  categoryId: string;
+  limit?: number;
+  searchParams?: any;
+}) => {
   const wixClient = await wixClientServer();
 
-  const res = await wixClient.products.queryProducts().eq("collectionIds",categoryId).limit(limit || PRODUCT_PER_PAGE).find();
+  const productQuery = wixClient.products
+    .queryProducts()
+    .startsWith("name", searchParams?.name || "")
+    .eq("collectionIds", categoryId)
+    .hasSome(
+      "productType",
+      searchParams?.type ? [searchParams.type] : ["physical", "digital"]
+    )
+    .gt("priceData.price", searchParams?.min || 0)
+    .lt("priceData.price", searchParams?.max || 999999)
+    .limit(limit || PRODUCT_PER_PAGE)
+    // .skip(
+    //   searchParams?.page
+    //     ? parseInt(searchParams.page) * (limit || PRODUCT_PER_PAGE)
+    //     : 0
+    // );
+  // .find();
 
-  // console.log(res.items[0].priceData)
+  if (searchParams?.sort) {
+    const [sortType, sortBy] = searchParams.sort.split(" ")
+
+    if (sortType === "asc") {
+      productQuery.ascending(sortBy);
+    }
+    if (sortType === "desc") {
+      productQuery.descending(sortBy);
+    }
+  }
+
+  const res = await productQuery.find();
+
   return (
-<div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap">
+    <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap">
       {res.items.map((product: products.Product) => (
         <Link
           href={"/" + product.slug}
@@ -42,7 +77,7 @@ const ProductList = async ({categoryId, limit, searchParams}:{categoryId:string;
           </div>
           <div className="flex justify-between">
             <span className="font-medium">{product.name}</span>
-            <span className="font-semibold">${product.priceData?.price}</span>
+            <span className="font-semibold">${product.price?.price}</span>
           </div>
           {product.additionalInfoSections && (
             <div
