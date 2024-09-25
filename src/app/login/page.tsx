9bbd3df9e31e00e85fad51ee8a8e3cb7daa64ae1
N.Guy <ngabonzizaguy1996@gmail.com>
2024-09-25 20:52:18 +0200

    page account modes(register, login, verification)

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,45 +1,96 @@
-"use client"
+"use client";
 
-import { useWixClient } from "@/hooks/useWixClient"
-import { useState } from "react"
+import { useWixClient } from "@/hooks/useWixClient";
+import { useState } from "react";
 
 enum MODE {
   LOGIN = "LOGIN",
   REGISTER = "REGISTER",
   RESET_PASSWORD = "RESET_PASSWORD",
-  EMAIL_VERIFICATION = "EMAIL_VERIFICATION"
+  EMAIL_VERIFICATION = "EMAIL_VERIFICATION",
 }
 
 const LoginPage = () => {
+  const [mode, setMode] = useState(MODE.LOGIN);
 
-  const [mode, setMode] = useState(MODE.LOGIN)
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailCode, setEmailCode] = useState("");
+  const [isLoading, setIsLoading] = useState("");
+  const [error, setError] = useState("");
+  const [message, setMessage] = useState("");
 
-  const [username, setUsername] = useState("")
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [emailCode, setEmailCode] = useState("")
-  const [isLoading, setIsLoading] = useState("")
-  const [error, setError] = useState("")
-  const [message, setMessage] = useState("")
+  const formTitle =
+    mode === MODE.LOGIN
+      ? "Log in"
+      : mode === MODE.REGISTER
+      ? "Register"
+      : mode === MODE.RESET_PASSWORD
+      ? "Reset your password"
+      : "Verify your Email";
+  const buttonTitle =
+    mode === MODE.LOGIN
+      ? "Login"
+      : mode === MODE.REGISTER
+      ? "Register"
+      : mode === MODE.RESET_PASSWORD
+      ? "Reset"
+      : "Verify";
 
-  const formTitle = mode === MODE.LOGIN ? "Log in" : mode === MODE.REGISTER ? "Register" : mode === MODE.RESET_PASSWORD ? "Reset your password" : "Verify your Email"
-  const buttonTitle = mode === MODE.LOGIN ? "Login" : mode === MODE.REGISTER ? "Register" : mode === MODE.RESET_PASSWORD ? "Reset" : "Verify"
-
-  const wixClient = useWixClient()
+  const wixClient = useWixClient();
 
   return (
-    <div className='h-[calc(100vh - 80px)] px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 flex items-center justify-center'>
+    <div className="h-[calc(100vh - 80px)] px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 flex items-center justify-center">
       <form className="flex flex-col gap-8">
         <h1 className="text-2sl font-semibold">{formTitle}</h1>
         {mode === MODE.REGISTER ? (
           <div className="flex flex-col gap-2">
             <label className="text-sm text-gray-700">Username</label>
-            <input type="text" name="username" placeholder="guy" className="ring-2 ring-gray-300 rounded-md p-4" />
+            <input
+              type="text"
+              name="username"
+              placeholder="username"
+              className="ring-2 ring-gray-300 rounded-md p-4"
+            />
           </div>
         ) : null}
+        {mode !== MODE.EMAIL_VERIFICATION ? (
+          <div className="flex flex-col gap-2">
+            <label className="text-sm text-gray-700">E-mail</label>
+            <input
+              type="email"
+              name="email"
+              placeholder="email@email.com"
+              className="ring-2 ring-gray-300 rounded-md p-4"
+            />
+          </div>
+        ) : (
+          <div className="flex flex-col gap-2">
+            <label className="text-sm text-gray-700">Verification Code</label>
+            <input
+              type="text"
+              name="emailCode"
+              placeholder="Code"
+              className="ring-2 ring-gray-300 rounded-md p-4"
+            />
+          </div>
+        )}
+        {mode === MODE.LOGIN || mode === MODE.REGISTER ? (
+          <div className="flex flex-col gap-2">
+          <label className="text-sm text-gray-700">Password</label>
+          <input
+            type="password"
+            name="password"
+            placeholder="Enter your password"
+            className="ring-2 ring-gray-300 rounded-md p-4"
+          />
+        </div>
+        ) : null}
+        <button className="bg-theme1 text-white p-2 rounded-t-md disabled:bg-pink-200 disabled:cursor-not-allowed">{buttonTitle}</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
