5fcc16248f632f1d64e69c77fc59f5633e2dd768
N.Guy <ngabonzizaguy1996@gmail.com>
2024-09-26 17:14:38 +0200

    authentication with wix's-managed login option

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -17,7 +17,7 @@ const LoginPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [emailCode, setEmailCode] = useState("");
-  const [isLoading, setIsLoading] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
   const [message, setMessage] = useState("");
 
@@ -41,9 +41,9 @@ const LoginPage = () => {
   const wixClient = useWixClient();
 
   return (
-    <div className="h-[calc(100vh - 80px)] px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 flex items-center justify-center">
+    <div className="h-[calc(100vh-80px)] px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 flex items-center justify-center">
       <form className="flex flex-col gap-8">
-        <h1 className="text-2sl font-semibold">{formTitle}</h1>
+        <h1 className="text-2xl font-semibold">{formTitle}</h1>
         {mode === MODE.REGISTER ? (
           <div className="flex flex-col gap-2">
             <label className="text-sm text-gray-700">Username</label>
@@ -78,16 +78,43 @@ const LoginPage = () => {
         )}
         {mode === MODE.LOGIN || mode === MODE.REGISTER ? (
           <div className="flex flex-col gap-2">
-          <label className="text-sm text-gray-700">Password</label>
-          <input
-            type="password"
-            name="password"
-            placeholder="Enter your password"
-            className="ring-2 ring-gray-300 rounded-md p-4"
-          />
-        </div>
+            <label className="text-sm text-gray-700">Password</label>
+            <input
+              type="password"
+              name="password"
+              placeholder="Enter your password"
+              className="ring-2 ring-gray-300 rounded-md p-4"
+            />
+          </div>
         ) : null}
-        <button className="bg-theme1 text-white p-2 rounded-t-md disabled:bg-pink-200 disabled:cursor-not-allowed">{buttonTitle}</button>
+        {mode === MODE.LOGIN && (
+          <div className="text-sm underline cursor-pointer" onClick={() => setMode(MODE.RESET_PASSWORD)}>
+            Forgot Password?
+          </div>
+        )}
+        <button
+          className="bg-theme1 text-white p-2 rounded-md disabled:bg-pink-200 disabled:cursor-not-allowed"
+          disabled={isLoading}
+        >
+          {isLoading ? "Loading..." : buttonTitle}
+        </button>
+        {error && <div className="text-red-600">{error}</div>}
+        {mode === MODE.LOGIN && (
+          <div className="text-sm underline cursor-pointer" onClick={() => setMode(MODE.REGISTER)}>
+            {"Don't"} have an account?
+          </div>
+        )}
+        {mode === MODE.REGISTER && (
+          <div className="text-sm underline cursor-pointer" onClick={() => setMode(MODE.LOGIN)}>
+            Have an account?
+          </div>
+        )}
+        {mode === MODE.RESET_PASSWORD && (
+          <div className="text-sm underline cursor-pointer" onClick={() => setMode(MODE.LOGIN)}>
+            Go back to Login
+          </div>
+        )}
+        {message && <div className="text-green-600 text-sm">{message}</div>}
       </form>
     </div>
   );
