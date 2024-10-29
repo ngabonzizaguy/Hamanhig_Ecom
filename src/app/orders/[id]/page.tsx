c3105da7d8acff1a95a2be0343e71585f73a1dee
N.Guy <32185091+ngabonzizaguy@users.noreply.github.com>
2024-10-29 19:06:09 +0000

    order page

---
+++ src/app/orders/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const OrderPage = () => {
+  return (
+    <div className=''>
+      orderPage
+    </div>
+  )
+}
+
+export default OrderPage
--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,8 +1,28 @@
 "use client"
+import { useRouter, useSearchParams } from "next/navigation"
+import { useEffect } from "react"
 import Confetti from "react-confetti"
 
 
 const SuccessPage = () => {
+    const searchParams = useSearchParams()
+    const router = useRouter()
+
+    const orderId = searchParams.get("orderId")
+
+    useEffect(() => {
+        if(!orderId) return
+
+        const timer = setTimeout(() => {
+            router.push("/orders/"+orderId)
+        }, 5000)
+        
+        return () => {
+            clearTimeout(timer)
+        }
+    }, [orderId, router])
+
+
     return (
         <div className="flex flex-col gap-6 items-center justify-center h-[calc(100vh-180px)]">
             <Confetti width={2000} height={1000}/>
