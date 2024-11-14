538a27fd73e27a8592bc989eacc5c54dc3c8d23a
N.Guy <ngabonzizaguy1996@gmail.com>
2024-11-14 19:35:50 +0200

    auth-managed login

--- src/components/NavIcons.tsx
+++ src/components/NavIcons.tsx
@@ -16,13 +16,9 @@ const NavIcons = () => {
   const [isLoading, setIsLoading] = useState(false);
 
   const router = useRouter();
-  const pathName = usePathname();
-
-  const wixClient = useWixClient();
-  const isLoggedIn = wixClient.auth.loggedIn();
 
   // TEMPORARY
-  // const isLoggedIn = false;
+  const isLoggedIn = false;
 
   const handleProfile = () => {
     if (!isLoggedIn) {
@@ -34,35 +30,35 @@ const NavIcons = () => {
 
   // AUTH WITH WIX-MANAGED AUTH
 
-  // const wixClient = useWixClient();
+  const wixClient = useWixClient();
 
-  // const login = async () => {
-  //   const loginRequestData = wixClient.auth.generateOAuthData(
-  //     "http://localhost:3000"
-  //   );
+  const login = async () => {
+    const loginRequestData = wixClient.auth.generateOAuthData(
+      "https://hamanhigecom-4cdx--3000--f565b097.local-credentialless.webcontainer.io"
+    );
 
-  //   console.log(loginRequestData);
+    console.log(loginRequestData);
 
-  //   localStorage.setItem("oAuthRedirectData", JSON.stringify(loginRequestData));
-  //   const { authUrl } = await wixClient.auth.getAuthUrl(loginRequestData);
-  //   window.location.href = authUrl;
-  // };
-
-  const handleLogout = async () => {
-    setIsLoading(true);
-    Cookies.remove("refreshToken");
-    const { logoutUrl } = await wixClient.auth.logout(window.location.href);
-    setIsLoading(false);
-    setIsProfileOpen(false);
-    router.push(logoutUrl);
+    localStorage.setItem("oAuthRedirectData", JSON.stringify(loginRequestData));
+    const { authUrl } = await wixClient.auth.getAuthUrl(loginRequestData);
+    window.location.href = authUrl;
   };
+
+  // const handleLogout = async () => {
+  //   setIsLoading(true);
+  //   Cookies.remove("refreshToken");
+  //   const { logoutUrl } = await wixClient.auth.logout(window.location.href);
+  //   setIsLoading(false);
+  //   setIsProfileOpen(false);
+  //   router.push(logoutUrl);
+  // };
 
 
   const { cart, counter, getCart } = useCartStore();
 
-  useEffect(() => {
-    getCart(wixClient);
-  }, [wixClient, getCart]);
+  // useEffect(() => {
+  //   getCart(wixClient);
+  // }, [wixClient, getCart]);
 
   return (
     <div className="flex items-center gap-4 xl:gap-6 relative">
@@ -72,13 +68,13 @@ const NavIcons = () => {
         width={22}
         height={22}
         className="cursor-pointer"
-        // onClick={login}
-        onClick={handleProfile}
+        onClick={login}
+        // onClick={handleProfile}
       />
       {isProfileOpen && (
         <div className="absolute p-4 rounded-md top-12 left-0 bg-white text-sm shadow-[0_3px_10px_rgb(0,0,0,0.2)] z-20">
           <Link href="/profile">Profile</Link>
-          <div className="mt-2 cursor-pointer" onClick={handleLogout}>
+          <div className="mt-2 cursor-pointer">
             {isLoading ? "Logging out" : "Logout"}
           </div>
         </div>
